a0b065d51c11b544a3ad32b25edd73329184e7e1
MarvinJoel45 <joelmarvinjara@gmail.com>
2024-05-23 11:39:11 -0500

    Ultimos retoques al Activity Register

--- app/src/main/java/com/example/appexamparcial/register.java
+++ app/src/main/java/com/example/appexamparcial/register.java
@@ -1,9 +1,11 @@
 package com.example.appexamparcial;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
@@ -12,9 +14,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+
 public class register extends AppCompatActivity {
 
     private Spinner spinner;
+    private EditText editTextDate;
+    private Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +34,14 @@ public class register extends AppCompatActivity {
         });
 
         spinner = findViewById(R.id.spinner);
+        editTextDate = findViewById(R.id.txt_date);
+        calendar = Calendar.getInstance();
 
-        String[] opciones = {"IronMan","Thor","Hulk","Spiderman"};
+        editTextDate.setOnClickListener(v -> mostrarCuadroFecha() );
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,opciones);
+        String[] options = {"IronMan","Thor","Hulk","Spiderman"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_options,options);
         spinner.setAdapter(adapter);
     }
 
@@ -40,6 +50,20 @@ public class register extends AppCompatActivity {
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
         finish();
+    }
+
+    private void mostrarCuadroFecha(){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+          this,
+                (view, year, month, dayOfMonth) -> {
+                    editTextDate.setText(dayOfMonth + "/" + (month+1) + "/" +year);
+                },
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+        datePickerDialog.show();
     }
 
 }
