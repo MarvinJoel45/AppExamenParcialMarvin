2b7eda6c38199ebd6a12a33726a2d008d7cb63c1
MarvinJoel45 <joelmarvinjara@gmail.com>
2024-05-23 01:08:25 -0500

    Segundo guardado

--- app/src/main/java/com/example/appexamparcial/register.java
+++ app/src/main/java/com/example/appexamparcial/register.java
@@ -3,6 +3,8 @@ package com.example.appexamparcial;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,24 +12,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class register extends AppCompatActivity {
+
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        spinner = findViewById(R.id.spinner);
+
+        String[] opciones = {"IronMan","Thor","Hulk","Spiderman"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,opciones);
+        spinner.setAdapter(adapter);
     }
 
-    public void Register(View view) {
+    public void Regresar(View view){
 
-        Intent i = new Intent(this, register.class);
+        Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
         finish();
     }
+
 }
